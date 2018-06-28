4612764f9879eecccaa015bddab2dd25c216fcd4
Aaron Lee <aaron.lee@oneteam.co.jp>
2018-06-28 15:06:57 +0900

    Add Carthage shared framework scheme

---
+++ ColorHash/ColorHash.h
@@ -0,0 +1,19 @@
+//
+//  ColorHash.h
+//  ColorHash
+//
+//  Created by Aaron Lee on 2018/06/28.
+//  Copyright © 2018 LittleApps Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorHash.
+FOUNDATION_EXPORT double ColorHashVersionNumber;
+
+//! Project version string for ColorHash.
+FOUNDATION_EXPORT const unsigned char ColorHashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorHash/PublicHeader.h>
+
+
